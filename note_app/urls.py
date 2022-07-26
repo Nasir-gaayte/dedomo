cac7c7d494c3238b41199d408ae3eea26af1884b
nasir-gaayte <slowboyz12@gmail.com>
2022-07-26 19:19:06 +0300

    after i add bootsrap

--- note_app/urls.py
+++ note_app/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
-from . import views
+from note_app import views
+
+
+urlpatterns = [
+    path('',views.index, name='index'), 
+    path('user/',views.user, name='user'),
+]
+
 
--- note_app/views.py
+++ note_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from requests import request
+
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'note_app/index.html')
+
+
+def user(request):
+    return render(request, 'note_app/user.html')
+    
